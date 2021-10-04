57860a17df4b9a595dfdf73d4de5f0ba5e8a42e5
wooong96 <smsw96@gmail.com>
2021-10-04 14:46:43 +0900

    Add Id Overlap
    Add Id Overlap

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -44,18 +44,30 @@ const validation = (data) => {
             userId: data.userId,
             password: data.password,
             checkPassword: data.checkPassword,
+            
             })  
             .then(res=>{
                 console.log(res.data)
+                swal.fire({
+                    icon: 'success',
+                    title: '회원 가입 완료.',
+                    text: data.userName + '님 저희 사이트에 오신걸 환영해요!', 
+                    confirmButtonText:'<a  href="/signin">로그인</a>',
+                }
+                );
                 })
+            .catch(error => {
+                swal.fire({
+                    icon: 'warning',
+                    title: '아이디 중복 오류',
+                    text: '아이디가 중복 되었습니다. 다시 입력해주세요', 
+                    cancelButtonText: '돌아가기',
+                }
+                );
+                console.log(error.response)
+            })
 
-        swal.fire({
-            icon: 'success',
-            title: '회원 가입 완료.',
-            text: '저희 사이트에 오신걸 환영합니다.', 
-            confirmButtonText:`<a class="btn btn-primary" href="/signin">로그인창으로 이동</a>`
-        },    
-        );
+                
         
         }
     return errors;
